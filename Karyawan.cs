90fa7dbe2883237b5a189afb940b24fcbed1da4f
arrafiidzaky <arrafiidzaky@gmail.com>
2019-05-03 15:40:16 +0700

    responsi 2312

---
+++ Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemrograman2244 { 
+public class Karyawan
+{
+    public Karyawan()
+    {
+    }
+    public Karyawan(string nik, string nama, int gajibulanan)
+    {
+        if (gajibulanan < 0)
+        {
+            Console.WriteLine("Gaji harus diatas 0!");
+        }
+        else
+        {
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+        }
+    }
+
+    public string Nik { get; set; }
+    public string Nama { get; set; }
+    public int GajiBulanan { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemrograman2244
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kry1 = new Karyawan();
+
+            kry1.Nik = "18112244";
+            kry1.Nama = "Hendri";
+            kry1.GajiBulanan = 2500000;
+
+            Karyawan kry2 = new Karyawan();
+
+            kry2.Nik = "10012001";
+            kry2.Nama = "samuel";
+            kry2.GajiBulanan = 3500000;
+
+            Console.WriteLine("No.\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("1.\t{0} {1}\t\t{2}", kry1.Nik, kry1.Nama, kry1.GajiBulanan);
+            Console.WriteLine("2.\t{0} {1}\t\t{2}", kry2.Nik, kry2.Nama, kry2.GajiBulanan);
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Asyikkkk gaji naik 10% !!!");
+            Console.WriteLine("\n");
+            Console.WriteLine("No.\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("1.\t{0} {1}\t\t{2}", kry1.Nik, kry1.Nama, kry1.GajiBulanan + (kry1.GajiBulanan * 0.10));
+            Console.WriteLine("2.\t{0} {1}\t\t{2}", kry2.Nik, kry2.Nama, kry2.GajiBulanan + (kry2.GajiBulanan * 0.10));
+
+            Console.ReadKey();
+        }
+    }
+}
